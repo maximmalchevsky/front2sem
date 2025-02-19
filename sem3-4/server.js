656ca56d5c7feac3f544eff3b2f58ce6b73ba3d4
maximmalchevsky <maximmalchevsky@yahoo.com>
2025-02-19 13:15:00 +0300

    sem3-4 done

---
+++ sem3-4/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
